50d845efaed6ce7da548eb00d95a24b1845398d9
z4n9 <archerlai93@gmail.com>
2023-11-27 19:24:44 +0600

    upd4

--- app/src/main/java/com/example/aaa/MainActivity.kt
+++ app/src/main/java/com/example/aaa/MainActivity.kt
@@ -20,34 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         Asus = ActivityMainBinding.inflate(layoutInflater)
         setContentView(Asus.root)
-        setContent {
-            AaaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "bye bye $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AaaTheme {
-        Greeting("Android")
-    }
 
 
-}
+
