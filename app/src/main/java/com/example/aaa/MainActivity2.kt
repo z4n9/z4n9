20b50c3c0624d39b83999e809b32a5214a814e1a
TheIn <zaynakzhol@gmail.com>
2023-11-29 21:45:30 +0600

    upd 15

--- app/src/main/java/com/example/aaa/MainActivity2.kt
+++ app/src/main/java/com/example/aaa/MainActivity2.kt
@@ -3,6 +3,7 @@ package com.example.aaa
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
@@ -23,17 +24,27 @@ class MainActivity2 : ComponentActivity() {
     }
     //кнопка перехода в 3 лист
 
-    fun OnClickListener(view: View){
+
+    fun onClickMain3(view: View) {
         var Artur = (R.id.Artur)
         var Rasul = (R.id.Rasul)
         var Nail = (R.id.Nail)
         var Zayn = (R.id.Zayn)
+        var Error = (R.id.Eror)
+        fun foo() {
+            listOf(Artur,Rasul,Nail,Zayn,Error).forEach{
+                if (it == )
+            }
+        }
 
 
-    }
-    fun onClickMain3(view: View){
-        val perehodActivity3 = Intent(this, MainActivity3::class.java)
-        startActivity(perehodActivity3)
-    }
+
+            val perehodActivity3 = Intent(this, MainActivity3::class.java)
+            startActivity(perehodActivity3)
+
+        }
+
+
+
 
 }
